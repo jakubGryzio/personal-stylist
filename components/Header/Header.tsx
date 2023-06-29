c8882a901d32263747e259759f20170d3ffe2d26
jgryzio <86599920+JakubGryzioHex@users.noreply.github.com>
2023-06-29 22:43:52 +0200

    Menu development

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import type { NextComponentType } from "next";
 import classes from "./header.module.css";
-import { Container } from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 import Image from "next/image";
 import telegramIcon from "../../public/telegram.svg";
 import tikTokIcon from "../../public/tiktok.svg";
@@ -18,7 +18,7 @@ const Header: NextComponentType = () => {
   };
 
   return (
-    <header className={`${classes.header}`}>
+    <Fragment>
       <Slide direction="left" in={showed} mountOnEnter unmountOnExit>
         <div className={`${classes.navigation__menu}`}>
           <header className={`${classes.header}`}>
@@ -41,39 +41,56 @@ const Header: NextComponentType = () => {
               </div>
             </Container>
           </header>
-          <h1>ПРО МЕНЕ</h1>
-          <h1>ПОСЛУГИ</h1>
-          <h1>КОНТАКТИ</h1>
+          <Container>
+            <Row>
+              <Col lg="6" md="6" className="text-center">
+                <p>ПРО МЕНЕ</p>
+                <p>ПОСЛУГИ</p>
+                <p>КОНТАКТИ</p>
+              </Col>
+              <Col lg="6" md="6" className="text-center">
+                <h1>ПРО МЕНЕ</h1>
+                <h1>ПОСЛУГИ</h1>
+                <h1>КОНТАКТИ</h1>
+              </Col>
+            </Row>
+          </Container>
         </div>
       </Slide>
-      <Container>
-        <div className={`${classes.nav__wrapper}`}>
-          <div className={`${classes.name}`}>
-            <p>Устина Скрипець</p>
-            <p style={{ color: "#D39C50" }}>персональний стиліст</p>
-          </div>
-          <div className={`${classes.nav__menu}`}>
-            <div className="d-flex alin-items-center gap-3 mb-0">
-              <Image priority src={telegramIcon} alt="Follow me on Telegram" />
-              <Image priority src={tikTokIcon} alt="Follow me on TikTok" />
-              <Image
-                priority
-                src={instagramIcon}
-                alt="Follow me on Instagram"
-              />
+      <header className={`${classes.header}`}>
+        <Container>
+          <div className={`${classes.nav__wrapper}`}>
+            <div className={`${classes.name}`}>
+              <p>Устина Скрипець</p>
+              <p style={{ color: "#D39C50" }}>персональний стиліст</p>
             </div>
-            <div className={`${classes.menu}`}>
-              <Image
-                onClick={showedHandler}
-                priority
-                src={menuIcon}
-                alt="Follow me on Telegram"
-              />
+            <div className={`${classes.nav__menu}`}>
+              <div className="d-flex alin-items-center gap-3 mb-0">
+                <Image
+                  priority
+                  src={telegramIcon}
+                  alt="Follow me on Telegram"
+                />
+                <Image priority src={tikTokIcon} alt="Follow me on TikTok" />
+                <Image
+                  priority
+                  src={instagramIcon}
+                  alt="Follow me on Instagram"
+                />
+              </div>
+              <div className={`${classes.menu}`}>
+                <Image
+                  onClick={showedHandler}
+                  priority
+                  src={menuIcon}
+                  alt="Follow me on Telegram"
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </Container>
-    </header>
+        </Container>
+      </header>
+    </Fragment>
   );
 };
 
