2cbe3ecccf25b95b67f71407ae91490b42459ecb
jakubGryzio <jakubgryzio@gmail.com>
2023-07-08 13:47:21 +0200

    Footer development

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,8 +1,23 @@
-import { NextComponentType } from "next";
 import React from "react";
+import { Col, Container, Row } from "reactstrap";
+import classes from "./footer.module.css";
 
-const Footer: NextComponentType = () => {
-  return <div></div>;
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className={classes.footer}>
+      <Container>
+        <Row>
+          <Col lg="12" className={classes.footer__top}>
+            <div className={classes.footer__copyright}>
+              <p>&copy; Copyright {year} - developed by Jakub Gryzio</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
